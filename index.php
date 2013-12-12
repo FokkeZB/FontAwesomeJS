39f4fa6e1e1cbe125762e1742db9210b2cccc38b
Fokke Zandbergen <mail@fokkezb.nl>
2013-12-12 20:42:04 +0100

    Removed header in generated file

--- index.php
+++ index.php
@@ -17,29 +17,7 @@ try {
 
 		$ln = count($icons);
 
-		$fa = '';
-
-		$fa .= "/*******************************************************************************************" . PHP_EOL;
-		$fa .= " * ABOUT" . PHP_EOL;
-		$fa .= " * This is a CommonJS module to use FontAwesome's icons in a Titanium app." . PHP_EOL;
-		$fa .= " * This file at http://fa.fokkezb.nl will automatically be up-to-date with FA." . PHP_EOL;
-		$fa .= " *" . PHP_EOL;
-		$fa .= " * QUICKSTART" . PHP_EOL;
-		$fa .= " * 1. Save the FontAwesome OTF to 'app/assets/fonts' or 'Resources/fonts':" . PHP_EOL;
-		$fa .= " *    https://github.com/FortAwesome/Font-Awesome/blob/master/fonts/FontAwesome.otf?raw=true" . PHP_EOL;
-		$fa .= " * 2. Save this file to your app's 'app/lib' or 'Resources' folder." . PHP_EOL;
-		$fa .= " * 2. Require it where you need it: var fa = require('fa');" . PHP_EOL;
-		$fa .= " *    Or store it as a global: Alloy.Globals.fa = require('fa');" . PHP_EOL;
-		$fa .= " * 3. Use it with 'fontFamily' on a label (won't work on buttons):" . PHP_EOL;
-		$fa .= " *    '.myLabel': {" . PHP_EOL;
-		$fa .= " *        font: {" . PHP_EOL;
-		$fa .= " *            fontFamily: 'FontAwesome'" . PHP_EOL;
-		$fa .= " *        }," . PHP_EOL;
-		$fa .= " *        text: Alloy.Globals.fa.volumeUp" . PHP_EOL;
-		$fa .= " *    }" . PHP_EOL;
-		$fa .= " ******************************************************************************************/" . PHP_EOL;
-		$fa .= PHP_EOL;
-		$fa .= "module.exports = {" . PHP_EOL;
+		$fa = "module.exports = {" . PHP_EOL;
 
 		foreach (array_keys($icons) as $i => $icon) {
 			if (strpos($icon, '-') !== false) {
